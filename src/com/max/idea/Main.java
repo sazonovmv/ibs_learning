e2b65a92b25410763c62bc7f18555130ddcb1908
admin <smi4i@yandex.ru>
2024-11-12 22:36:10 +0300

    Homework 3

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,14 +1,43 @@
 package com.max.idea;
-
-
 import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        Random rand = new Random();
-        double maxValue = rand.nextDouble();
-        byte minValue = Byte.MIN_VALUE;
-        System.out.println(maxValue);
-        System.out.println(minValue);
+
+        int arrayLenght = 5;
+        int [] array = new int[arrayLenght];
+        array = rndArray(arrayLenght);
+        prntArray(array);
+        System.out.println(" - Это массив");
+        elmSwap(array);
+        prntArray(array);
+        System.out.println(" - Это массив после смены местами первого и последнего элемента");
+        System.out.println(array[0]+ " + "+ array[2]+ " = " + (array[0]+array[2]));
     }
+    public static int [] rndArray(int arrayLenght)
+    {
+        int [] array = new int[arrayLenght];
+        Random rand = new Random();
+        for (int i=0;i<array.length;i++)
+        {
+            array[i] = rand.nextInt(99);
+        }
+             return array;
+    }
+    public static void prntArray(int [] array)
+    {
+        for (int i=0;i<array.length;i++)
+        {
+            System.out.print(array[i]+" ");
+        }
+
+    }
+
+    public static int [] elmSwap(int [] arrayToSwap)
+    {
+        int elmSwap = arrayToSwap[0];
+        arrayToSwap[0] = arrayToSwap[4];
+        arrayToSwap[4] = elmSwap;
+        return arrayToSwap;
+    }
+
 }
