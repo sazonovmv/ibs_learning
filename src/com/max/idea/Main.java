b83d237348475010310e71fdad4c7b65e1be8c37
admin <smi4i@yandex.ru>
2024-11-12 20:29:11 +0300

    Homework 2

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,14 @@
 package com.max.idea;
 
+
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Random rand = new Random();
+        double maxValue = rand.nextDouble();
+        int minValue = Integer.MIN_VALUE;
+        System.out.println(maxValue);
+        System.out.println(minValue);
     }
 }
