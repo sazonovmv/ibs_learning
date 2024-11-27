37f49dfebcaf8ec39ae1b8401a0df30d7b718680
admin <smi4i@yandex.ru>
2024-11-28 01:31:49 +0300

    Homework 5 exercise2

--- src/com/max/idea/HomeWork4.java
+++ src/com/max/idea/HomeWork4.java
@@ -1,47 +1,24 @@
 package com.max.idea;
 
-
 import java.util.Scanner;
 
 public class HomeWork4 {
     public static void main(String[] args) {
-        final int x,y,z;
-        x = 1;
-        y = 4;
-        z = 7;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите размер массива");
-        int arrayLenth = scanner.nextInt();
-        int [] array = fullArray(new int[arrayLenth]);
-        //printArray(array); - првоерка корректности
-        findElement(array,x,y,z);
+        System.out.println("Введите любое положительное целое число");
+        int number = scanner.nextInt();
+        factorialNumber(number);
     }
 
-    public static int [] fullArray(int [] emptyArray)
-    {
-        Scanner scanner = new Scanner(System.in);
-        for (int i=0;i <= emptyArray.length-1;i++)
-        {
-            System.out.println("Введите значение элемента массива");
-            emptyArray[i] = scanner.nextInt();
-        }
-        return emptyArray;
-    }
-//    public static void printArray(int [] array)
-//    {
-//        for (int i : array) {
-//            System.out.print(i + " ");
-//        }
-//
-//    }
-    public static void findElement(int [] array, int x, int y, int z)
-    {
-        for (int i : array) {
-            if (array[i]==x | array[i]==y | array[i]==z)
-            {
-                System.out.println("Данное значение имеется в константах");
-                break;
+    public static void factorialNumber(int number) {
+        int sum = 0;
+        if (number > 0) {
+            for (int i = 1; i <= number; i++) {
+                sum = sum + i;
             }
+            System.out.println(sum);
+        } else {
+            System.out.println("Вы ввели неподходящее число");
         }
     }
 }
