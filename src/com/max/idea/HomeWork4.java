7ca50f95ad7abfd1e7fc522df09f188d6cbf2582
sazonovmv <105490061+sazonovmv@users.noreply.github.com>
2024-12-06 21:36:56 +0300

    Update HomeWork4.java
    new ver

--- src/com/max/idea/HomeWork4.java
+++ src/com/max/idea/HomeWork4.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 
 public class HomeWork4 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+      Scanner scanner = new Scanner(System.in);
         System.out.println("Введите строку!");
         String example = scanner.nextLine();
-        while(!example.matches("[a-zA-Z ]+")) {
-            System.out.println("Строка состоит не только из латиницы, повторите ввод");
-            example = scanner.nextLine();
-        }
         String [] result = example.split(" ");
-        System.out.println(result.length);
+        int wordCount = 0; 
+        for (String word : result) 
+        { 
+            if (word.matches("[а-яА-Я]+")) 
+            { 
+                wordCount++;} 
+            } 
+        System.out.println("Количество слов, содержащих только кириллицу: " + wordCount);
     }
 }
