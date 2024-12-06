7617193592484a5af46ded1aa6efee649c201287
admin <smi4i@yandex.ru>
2024-12-06 10:37:11 +0300

    Homework final exercise3(ver1)

--- src/com/max/idea/HomeWork4.java
+++ src/com/max/idea/HomeWork4.java
@@ -5,33 +5,41 @@ import java.util.Scanner;
 public class HomeWork4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int count = 1;
+        int count = 0;
         System.out.println("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
         while (count!=3) {
             String answer = scanner.nextLine();
-            if (answer.equalsIgnoreCase("Подсказка") && count ==1)
+            if (answer.equalsIgnoreCase("Подсказка") && count == 0)
             {
                 System.out.println("Это связано с компьютерной техникой и болезнями человека");
-                continue;
+
             }
-            else if (answer.equalsIgnoreCase("Подсказка") && count > 1)
+            else if (answer.equalsIgnoreCase("Подсказка") && count > 0)
             {
                 System.out.println("Подсказка недоступна");
-                continue;
+
             }
             else if (answer.equalsIgnoreCase("Заархивированный вирус")) {
                 System.out.println("Правильно!");
                 break;
             }
-            else
+            else if(!answer.equalsIgnoreCase("Заархивированный вирус"))
+
             {
-                System.out.println("Подумай еще!");
+                count++;
+               if (count <3)
+               {
+                   System.out.println("Подумай еще!");
+               }
+               else {
+
+                   System.out.println("Обидно, приходи в другой раз");
+               }
             }
-            count++;
+
         }
-        if (count==3) {
-            System.out.println("Обидно, приходи в другой раз");
-        }
+
+
     }
 
 
