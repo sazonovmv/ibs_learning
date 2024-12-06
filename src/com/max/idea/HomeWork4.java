3762c595da39d4a3a38ab3771e6edbdfada1cb49
admin <smi4i@yandex.ru>
2024-12-06 10:52:15 +0300

    Homework final exercise0(ver1)

--- src/com/max/idea/HomeWork4.java
+++ src/com/max/idea/HomeWork4.java
@@ -5,42 +5,12 @@ import java.util.Scanner;
 public class HomeWork4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int count = 0;
-        System.out.println("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
-        while (count!=3) {
-            String answer = scanner.nextLine();
-            if (answer.equalsIgnoreCase("Подсказка") && count == 0)
-            {
-                System.out.println("Это связано с компьютерной техникой и болезнями человека");
-
-            }
-            else if (answer.equalsIgnoreCase("Подсказка") && count > 0)
-            {
-                System.out.println("Подсказка недоступна");
-
-            }
-            else if (answer.equalsIgnoreCase("Заархивированный вирус")) {
-                System.out.println("Правильно!");
-                break;
-            }
-            else if(!answer.equalsIgnoreCase("Заархивированный вирус"))
-
-            {
-                count++;
-               if (count <3)
-               {
-                   System.out.println("Подумай еще!");
-               }
-               else {
-
-                   System.out.println("Обидно, приходи в другой раз");
-               }
-            }
-
-        }
-
-
+        System.out.println("Введите текущий курс: 1 USD to RUB");
+        Double usd = scanner.nextDouble();
+        System.out.println("Введите сумму рублей к переводу");
+        Double rubCount = scanner.nextDouble();
+        double sum = rubCount/usd;
+        sum = Math.ceil (sum * 100.0) / 100.0;
+        System.out.println(sum);
     }
-
-
 }
