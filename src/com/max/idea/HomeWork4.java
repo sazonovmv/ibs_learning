d8fe9c1c0c7fa93d29830ca103283dedb81689d6
admin <smi4i@yandex.ru>
2024-12-06 15:14:13 +0300

    Homework work with date 3(ver 1)

--- src/com/max/idea/HomeWork4.java
+++ src/com/max/idea/HomeWork4.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class HomeWork4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите строку");
+        System.out.println("Введите строку!");
         String example = scanner.nextLine();
         String [] result = example.split(" ");
         System.out.println(result.length);
