859406bcc66f4834c639b0a5b90e4318e85af298
admin <smi4i@yandex.ru>
2024-12-06 10:22:29 +0300

    Homework final exercise3(ver1)

--- src/com/max/idea/HomeWork4.java
+++ src/com/max/idea/HomeWork4.java
@@ -5,47 +5,34 @@ import java.util.Scanner;
 public class HomeWork4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите количество строк");
-        int count = scanner.nextInt();
-        Scanner scannerString = new Scanner(System.in);
-        String[] lines = new String[count];
-        for (int i = 0; i < count; i++) {
-            System.out.println("Введите значение строки");
-            lines[i] = scannerString.nextLine();
+        int count = 1;
+        System.out.println("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
+        while (count!=3) {
+            String answer = scanner.nextLine();
+            if (answer.equalsIgnoreCase("Подсказка") && count ==1)
+            {
+                System.out.println("Это связано с компьютерной техникой и болезнями человека");
+                continue;
+            }
+            else if (answer.equalsIgnoreCase("Подсказка") && count > 1)
+            {
+                System.out.println("Подсказка недоступна");
+                continue;
+            }
+            else if (answer.equalsIgnoreCase("Заархивированный вирус")) {
+                System.out.println("Правильно!");
+                break;
+            }
+            else
+            {
+                System.out.println("Подумай еще!");
+            }
+            count++;
         }
-        System.out.println(lines[countSameElements(lines)]);
+        if (count==3) {
+            System.out.println("Обидно, приходи в другой раз");
+        }
     }
 
-    public static int countSameElements(String[] array) {
-        int maxValue = 0;
-        int maxValueString = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            int uniqueCount = 0;
-            char[] chrArray = array[i].toCharArray();
-
-            for (int j = 0; j < chrArray.length; j++) {
-                boolean isUnique = true;
-
-                for (int k = j + 1; k < chrArray.length; k++) {
-                    if (chrArray[j] == chrArray[k]) {
-                        isUnique = false;
-                        break;
-                    }
-                }
-
-                if (isUnique) {
-                    uniqueCount++;
-                }
-            }
-
-            if (maxValue < uniqueCount) {
-                maxValue = uniqueCount;
-                maxValueString = i;
-            }
-        }
-
-        return maxValueString;
-    }
 
 }
